c205984e82a75d78df20955c4dff939b70614467
Brendan Dileo <dileob23@gmail.com>
2025-08-12 19:21:47 -0400

    Update HomeController

--- src/main/java/com/brendanddev/learning_path_api/controller/HomeController.java
+++ src/main/java/com/brendanddev/learning_path_api/controller/HomeController.java
@@ -1,5 +1,22 @@
 package com.brendanddev.learning_path_api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Defines the HomeController that provides a simple endpoint to check if the API is running
+ */
+@RestController
 public class HomeController {
+
+    /**
+     * Endpoint to check if the API is up and running
+     * 
+     * @return A simple message indicating the API status
+     */
+    @GetMapping("/")
+    public String home() {
+        return "API up and running!";
+    }
     
 }
