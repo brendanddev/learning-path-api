479734439ea5e194e5898e5027099321abe2d88a
Brendan Dileo <dileob23@gmail.com>
2025-08-12 11:39:01 -0400

    Add getters and setters for User entity

--- src/main/java/com/brendanddev/learning_path_api/model/User.java
+++ src/main/java/com/brendanddev/learning_path_api/model/User.java
@@ -51,13 +51,33 @@ public class User {
     public Long getId() {
         return id;
     }
-    
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
+    public Set<Skill> getSkills() {
+        return skills;
+    }
 
-    
+    public void setSkills(Set<Skill> skills) {
+        this.skills = skills;
+    }
+
 }
