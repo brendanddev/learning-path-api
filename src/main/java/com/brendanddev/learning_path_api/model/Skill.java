125bd8c47f73db61d1fe13cc7daa1c65efa00f6a
Brendan Dileo <dileob23@gmail.com>
2025-08-12 11:44:46 -0400

    Add fields to Skill entity

--- src/main/java/com/brendanddev/learning_path_api/model/Skill.java
+++ src/main/java/com/brendanddev/learning_path_api/model/Skill.java
@@ -1,16 +1,32 @@
 package com.brendanddev.learning_path_api.model;
 
+import java.util.Set;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
-
-
-
-
-
-
-
-
-
-
+/**
+ * Represents a skill or topic that users can learn.
+ */
+@Entity
 public class Skill {
+
+    /**
+     * The name of the skill, which serves as the primary key.
+     */
+    @Id
+    private String name;
+
+    /**
+     * Users who have this skill.
+     * This is the inverse side of the many-to-many relationship with the User entity.
+     * Each skill can be associated with multiple users, and users can have multiple skills.
+     * The 'mappedBy = skills' attribute indicates that the ownership of the relationship is
+     * managed by the 'skills' field in the User entity.
+     */
+    @ManyToMany(mappedBy = "skills")
+    private Set<User> users;
+
+
     
 }
