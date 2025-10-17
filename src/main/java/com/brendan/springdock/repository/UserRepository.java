64e2b8e0f66a330aa9bcb91c05fd18ddacedcf0f
Brendan Dileo <dileob23@gmail.com>
2025-10-17 09:01:32 -0400

    Update UserRepository

--- src/main/java/com/brendan/springdock/repository/UserRepository.java
+++ src/main/java/com/brendan/springdock/repository/UserRepository.java
@@ -1,5 +1,20 @@
 package com.brendan.springdock.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.brendan.springdock.models.User;
+
+/**
+ * Repository interface providing CRUD operations for User entities.
+ * 
+ * It extends the 'JpaRepository' interface from Spring Data JPA, which automatically
+ * provides common database operations without the need for boilerplate code.
+ * The generic types 'User' and 'Long' specify the entity type and the type of its primary key, respectively.
+ * 
+ * Spring Data JPA will automatically provide the implementation at runtime.
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> { 
     
 }
