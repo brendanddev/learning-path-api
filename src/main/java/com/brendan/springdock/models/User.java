4d4ee2d07aa34bfbc4618f53dfcf599fff2286d0
Brendan Dileo <dileob23@gmail.com>
2025-10-17 08:47:59 -0400

    Setup User entity

--- src/main/java/com/brendan/springdock/models/User.java
+++ src/main/java/com/brendan/springdock/models/User.java
@@ -1,6 +1,34 @@
 package com.brendan.springdock.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+/**
+ * 
+ */
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+    private String email;
+    
+    // Required no-arg constructor
+    public User() { }
+
+    // All-args constructor
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+
     
 }
