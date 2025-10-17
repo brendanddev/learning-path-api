dc28f91a7bc2f29aadeb22b82a6c3d8a5982f0ff
Brendan Dileo <dileob23@gmail.com>
2025-10-17 08:34:43 -0400

    Create main app class

---
+++ src/main/java/com/brendan/springdock/SpringdockApplication.java
@@ -0,0 +1,13 @@
+package com.brendan.springdock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brendan/springdock/SpringdockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brendan.springdock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
