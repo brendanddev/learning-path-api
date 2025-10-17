d0b27761e97c71e3e7efd7a455c93b3c863a9491
Brendan Dileo <dileob23@gmail.com>
2025-10-17 09:07:49 -0400

    Setup UserController

--- src/main/java/com/brendan/springdock/controllers/UserController.java
+++ src/main/java/com/brendan/springdock/controllers/UserController.java
@@ -1,5 +1,18 @@
 package com.brendan.springdock.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brendan.springdock.services.UserService;
+
+@RestController
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    
     
 }
