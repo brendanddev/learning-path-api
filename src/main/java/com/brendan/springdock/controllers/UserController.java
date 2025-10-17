5295003e94bca4fcb16bea98adf69d7766ff27c2
Brendan Dileo <dileob23@gmail.com>
2025-10-17 09:13:57 -0400

    Add getAllUsers endpoint to UserController

--- src/main/java/com/brendan/springdock/controllers/UserController.java
+++ src/main/java/com/brendan/springdock/controllers/UserController.java
@@ -1,7 +1,10 @@
 package com.brendan.springdock.controllers;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +39,21 @@ public class UserController {
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User newUser = userService.createUser(user.getName(), user.getEmail());
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
+
+    /**
+     * Retrieves all Users from the system.
+     * 
+     * HTTP GET /users
+     * Returns a list of all User entities stored in the database.
+     * The response contains HTTP 200 (OK) and the list of users in JSON format.
+     * 
+     * @return ResponseEntity containing the list of Users and HTTP status
+     */
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
     }
 
 
