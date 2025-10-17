fed0fdf2cdda3e21ed68c2bbe7fb286db350789c
Brendan Dileo <dileob23@gmail.com>
2025-10-17 09:47:54 -0400

    Update CoreController

--- src/main/java/com/brendan/springdock/controllers/CoreController.java
+++ src/main/java/com/brendan/springdock/controllers/CoreController.java
@@ -1,5 +1,27 @@
 package com.brendan.springdock.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Defines the Core Controller for general, application-wide endpoints not specific to a domain entity.
+ * 
+ * In Spring Boot applications, Controllers handle HTTP requests and map them to Java methods, and return responses. 
+ */
+@RestController
 public class CoreController {
+
+    /**
+     * Default home endpoint.
+     * 
+     * GET /
+     * Returns a simple welcome message.
+     * 
+     * @return A welcome message
+     */
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to SpringDock!";
+    }
     
 }
