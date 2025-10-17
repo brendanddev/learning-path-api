a59ffdd886e10e59524a4caffcf5f1ecd984af09
Brendan Dileo <dileob23@gmail.com>
2025-10-17 10:14:04 -0400

    Update SkillRepository

--- src/main/java/com/brendan/springdock/repository/SkillRepository.java
+++ src/main/java/com/brendan/springdock/repository/SkillRepository.java
@@ -1,6 +1,14 @@
 package com.brendan.springdock.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface SkillRepository {
+import com.brendan.springdock.models.Skill;
+
+/**
+ * Repository interface providing CRUD operations for Skill entities.
+ */
+@Repository
+public interface SkillRepository extends JpaRepository<Skill, Long> {
     
 }
